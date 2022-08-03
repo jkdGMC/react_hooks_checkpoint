c19c83bf4c19039e67a5204e37ae5dac342073ae
Jules Ko DIOUF <jkd.gomycode@gmail.com>
2022-08-03 19:59:44 +0000

    Adding props in movieCard component

--- src/Components/MovieCard.js
+++ src/Components/MovieCard.js
@@ -1,13 +1,14 @@
 import React from 'react'
 import './MovieCard.css'
-export default function MovieCard() {
 
+export default function MovieCard(props) {
   return (
     <div className='movieCard_component'>
-      <img src="" alt="imgPoster" />
-      <div className='title_movie'>titre</div>
-      <div className='description_movie'>description</div>
-      <div className='rating_movie'>rating</div>
+      <img src={props.posterUrl} alt="imgPoster" />
+      <div className='title_movie'>{props.title}</div>
+      <div className='description_movie'><p>{props.description}</p></div>
+      <div className='rating_movie'>{props.rating}</div>
+      <div className="btn-more"><button>Voir plus</button></div>
     </div>
   )
 }
--- src/Components/MovieList.js
+++ src/Components/MovieList.js
@@ -2,11 +2,11 @@ import React from 'react'
 import MovieCard from './MovieCard'
 import './MovieList.css'
 
-export default function MovieList(props) {
+export default function MovieList() {
 
   return (
     <div className='movieList_component'>
-      <MovieCard posterUrl="" title="" description="" rating=""/>
+      <MovieCard posterUrl="" title="Breaking bad" description="Lorem ipsum, dolor sit amet consectetur adipisicing elit. Tenetur quam enim mollitia voluptates nesciunt rerum earum eum, obcaecati hic iure quisquam id iste amet corrupti perspiciatis placeat accusamus? Quos, ea!" rating=""/>
       <MovieCard posterUrl="" title="" description="" rating=""/>
       <MovieCard posterUrl="" title="" description="" rating=""/>
       <MovieCard posterUrl="" title="" description="" rating=""/>
